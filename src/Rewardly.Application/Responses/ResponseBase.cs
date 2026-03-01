4428e49740ef6304ca3322f21deb19799d1bf963
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-03-01 12:28:22 -0300

    first commit

---
+++ src/Rewardly.Application/Responses/ResponseBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Rewardly.Application.Responses;
+
+public class ResponseBase<T>
+{
+    public bool Success { get; init; }
+    public string? Message { get; init; }
+    public T? Data { get; init; }
+    public IEnumerable<string>? Errors { get; init; }
+}
