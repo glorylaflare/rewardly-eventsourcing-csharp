a2f8f2a17b13fdc515d7f4a591308b31fdde262e
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-03-15 15:11:36 -0300

    feat: Add IAggregateRoot interface for event sourcing structure #7

---
+++ src/Rewardly.Domain/Interfaces/v1/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Rewardly.Domain.Interfaces.v1;
+
+public interface IAggregateRoot
+{
+    Guid Id { get; }
+    int Version { get; }
+
+    IReadOnlyCollection<IEvent> GetUncommittedEvents();
+    void ClearUncommittedEvents();
+    void LoadFromHistory(IEnumerable<IEvent> events);
+}
