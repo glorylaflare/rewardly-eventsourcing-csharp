470557a23e6c3d75792e1b2da7381818962c3e00
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-03-15 13:59:28 -0300

    feat: Add RewardlyController and domain event structure with interfaces #5

---
+++ src/Rewardly.Domain/Interfaces/v1/IEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Rewardly.Domain.Interfaces.v1;
+
+public interface IEvent
+{
+    Guid AggregateId { get; }
+    DateTime OccurredAt { get; }
+    int Version { get; }
+}
---
+++ src/Rewardly.Domain/Interfaces/v1/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Rewardly.Domain.Interfaces.v1;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity> GetById(Guid id);
+    Task SaveAsync(TEntity entity);
+}
