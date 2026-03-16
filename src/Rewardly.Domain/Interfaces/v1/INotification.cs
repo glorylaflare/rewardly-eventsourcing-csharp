0e3928ef6435acc7e1158825c44ffd3bdb1da24c
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2026-03-16 19:59:20 -0300

    feat: Add Notification handling with INotification interface and NotificationContext class #10

---
+++ src/Rewardly.Domain/Interfaces/v1/INotification.cs
@@ -0,0 +1,13 @@
+﻿using Rewardly.Domain.Notifications.v1;
+
+namespace Rewardly.Domain.Interfaces.v1;
+
+public interface INotification
+{
+    IReadOnlyCollection<Notification> Notifications { get; }
+    bool HasNotifications { get; }
+
+    void AddNotification(string code, string message);
+    void AddNotification(Notification notification);
+    void AddNotification(IEnumerable<Notification> notifications);
+}
---
+++ src/Rewardly.Domain/Notifications/v1/NotificationContext.cs
@@ -0,0 +1,29 @@
+﻿using Rewardly.Domain.Interfaces.v1;
+
+namespace Rewardly.Domain.Notifications.v1;
+
+public sealed class NotificationContext : INotification
+{
+    private readonly List<Notification> _notifications = new();
+
+    public IReadOnlyCollection<Notification> Notifications
+        => _notifications;
+
+    public bool HasNotifications
+        => _notifications.Any();
+
+    public void AddNotification(string code, string message)
+    {
+        _notifications.Add(new Notification(code, message));
+    }
+
+    public void AddNotification(Notification notification)
+    {
+        _notifications.Add(notification);
+    }
+
+    public void AddNotification(IEnumerable<Notification> notifications)
+    {
+        _notifications.AddRange(notifications);
+    }
+}
