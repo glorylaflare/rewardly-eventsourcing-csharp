8495a622cd272312dfa1e84a8372ba7d05078d15
glorylaflare <marcelojunior_@outlook.com.br>
2026-08-03 19:38:03 -0300

    Refactor ApiControllerBase for async and flexible responses
    Simplified the constructor by removing the `ILogger` dependency.
    Replaced `Result<T>` with `ExecuteAsync<TResponse>` to support
    asynchronous execution and flexible HTTP status code handling.
    Updated response handling to use `StatusCode` for improved
    customization. Added `System.Net` namespace for HTTP status codes. #47

--- src/Rewardly.Api/Controllers/ApiControllerBase.cs
+++ src/Rewardly.Api/Controllers/ApiControllerBase.cs
@@ -1,24 +1,25 @@
 ﻿using Rewardly.Application.Responses;
 using Rewardly.Domain.Interfaces.v1;
+using System.Net;
 
 namespace Rewardly.Api.Controllers;
 
 public abstract class ApiControllerBase : ControllerBase
 {
     private readonly INotification _notification;
-    private readonly ILogger _logger;
 
-    protected ApiControllerBase(INotification notification, ILogger logger)
+    protected ApiControllerBase(INotification notification)
     {
         _notification = notification;
-        _logger = logger;
     }
 
-    protected IActionResult Result<T>(T data)
+    protected async Task<IActionResult> ExecuteAsync<TResponse>(Func<Task<TResponse>> request, HttpStatusCode statusCode) 
     {
-        if (_notification.HasNotifications)
-            return BadRequest(ResponseBase<T>.Fail(_notification.Notifications.Select(_ => _.Message).ToArray()));
+        TResponse data = await request();
 
-        return Ok(ResponseBase<T>.Ok(data));
+        if (_notification.HasNotifications)
+            return StatusCode((int)HttpStatusCode.BadRequest, ResponseBase<TResponse>.Fail(_notification.Notifications.Select(_ => _.Message).ToArray()));
+
+        return StatusCode((int)statusCode, ResponseBase<TResponse>.Ok(data));
     }
 }
